a0581f2a7648d3d67bcef1ce58900f981c8c07fd
Mark Fredrickson <mark.m.fredrickson@gmail.com>
2011-10-13 10:07:12 -0500

    starting an example folder, but ignoring the data as it is copyrighted

---
+++ test/examples/fetch-examples.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Besley, T. and Reynal-Querol, M. (2011) "Do Democracies Select More 
+# Educated Leaders?" American Political Science Review. 105(3): 516 -- 529 
+# (freely available at time of writing)
+
+wget -k -p "http://journals.cambridge.org/action/displayFulltext?type=6&fid=8368205&jid=PSR&volumeId=105&issueId=03&aid=8368204&fulltextType=RA&fileId=S0003055411000281"
+
+mv journals.cambridge.org besley-ryenal-querol-apsr
+
+
