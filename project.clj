ecbb13b8943d4c472c721ea504177b592d8a7a5d
Mark Fredrickson <mark.m.fredrickson@gmail.com>
2011-10-13 10:05:40 -0500

    clojure 1.3 was causing problems with swank/slime

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject publichouse "0.1.0"
   :description "Transform HTML to EPUB"
-  :dependencies [[clojure "1.3.0"]
+  :dependencies [[clojure "1.2.1"]
                  [nl.siegmann.epublib/epublib-core "3.0-SNAPSHOT"]
                  [enlive "1.0.0"]]
   :repositories
