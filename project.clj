13d1ac22f42f3964b98ed343e33a88ed0116197b
Mark Fredrickson <mark.m.fredrickson@gmail.com>
2011-10-12 21:44:23 -0500

    initial checkin

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject publichouse "0.1.0"
+  :description "Transform HTML to EPUB"
+  :dependencies [[clojure "1.3.0"]
+                 [nl.siegmann.epublib/epublib-core "3.0-SNAPSHOT"]
+                 [enlive "1.0.0"]]
+  :repositories
+  {"psiegman"
+   "https://github.com/psiegman/mvn-repo/raw/master/snapshots"})
+               
---
+++ src/publichouse/core.clj
@@ -0,0 +1,1 @@
+(ns publichouse.core)
